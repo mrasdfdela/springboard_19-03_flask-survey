c7112ec88976f687b9412f1a8cf0c7def9d9677a
Jerry Hsu <Jerry>
2021-05-05 22:37:24 -0400

    Step Three: The Question Page

--- app.py
+++ app.py
@@ -11,3 +11,8 @@ responses = []
 @app.route('/')
 def home_page():
     return render_template("index.html", survey=satisfaction_survey)
+
+@app.route('/questions/<int:idx>')
+def questions(idx):
+  question = satisfaction_survey.questions[idx]
+  return render_template("question.html",survey=satisfaction_survey,idx=idx, question=question)
